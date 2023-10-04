d45c68592578e1b25241680f51d08139a3749d3e
Tomer Horowitz <45624804+tomerh2001@users.noreply.github.com>
2023-10-04 22:10:03 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,36 +1,50 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import process from 'node:process';
 import Bun from 'bun';
 import {marked} from 'marked';
 import {emojify} from 'node-emoji';
 import nodemailer from 'nodemailer';
+import {name, version} from './package.json';
+
+function getConfig() {
+	return {
+		packageName: process.env.PACKAGE_NAME ?? name,
+		version: process.env.VERSION ?? version,
+		username: process.env.USERNAME,
+		password: process.env.PASSWORD,
+		from: process.env.FROM,
+		to: process.env.TO,
+	};
+}
 
 async function getChangelog() {
 	const changelog = await Bun.file('CHANGELOG.md').text();
 	return marked(emojify(changelog));
 }
 
+const config = getConfig();
+console.debug(config)
+
 const transporter = nodemailer.createTransport({
 	service: 'gmail',
 	auth: {
-		user: '',
-		pass: '',
+		user: config.username,
+		pass: config.password,
 	},
 });
-if (!await transporter.verify()) {
-	throw new Error('Invalid email transport configuration');
-}
 
 const changelog = await getChangelog();
 const mailOptions = {
-	from: '',
-	to: '',
-	subject: 'Changelog',
+	from: config.from,
+	to: config.to,
+	subject: `Release v${config.version} for ${config.packageName}`,
 	html: changelog,
 };
 
 transporter.sendMail(mailOptions, (error, info) => {
 	if (error) {
-		console.error(error);
-	} else {
-		console.log(`Email sent: ${info.response}`);
+		throw error;
 	}
+
+	console.log('Email sent', info.messageId, info.response, info.envelope);
 });
