05757fb03eaa43445db5ab333cf2f94cf56617ed
Tomer Horowitz <tomerh@realcommerce.co.il>
2023-10-05 01:16:13 +0300

    feat: :sparkles: added the option to get the changelog from the note of the head commit

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
-/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
 import process from 'node:process';
 import Bun from 'bun';
 import {marked} from 'marked';
 import {emojify} from 'node-emoji';
 import nodemailer from 'nodemailer';
+import git from 'simple-git';
 import {name, version} from './package.json';
 
 function getConfig() {
@@ -14,6 +19,7 @@ function getConfig() {
 		password: process.env.PASSWORD,
 		from: process.env.FROM,
 		to: process.env.TO,
+		sslVerify: process.env.SSL_VERIFY ?? false,
 	};
 }
 
@@ -22,9 +28,26 @@ async function getChangelog() {
 	return marked(emojify(changelog));
 }
 
-const config = getConfig();
-console.debug(config)
+async function getChangeLogFromNote() {
+	try {
+		const commitHash: string = await simpleGit.revparse(['HEAD']);
+		console.log('Latest commit hash:', commitHash.trim());
 
+		const noteHash = await simpleGit.raw(['notes', 'show']);
+		console.log('Note hash:', noteHash.trim());
+
+		const changelog = await simpleGit.raw(['notes', 'show', noteHash.trim()]);
+		console.log('Note:', changelog.trim());
+
+		return marked(emojify(changelog));
+	} catch (error) {
+		console.error(error);
+		return null;
+	}
+}
+
+const config = getConfig();
+const simpleGit = git({config: [`http.sslVerify=${config.sslVerify}`]});
 const transporter = nodemailer.createTransport({
 	service: 'gmail',
 	auth: {
@@ -33,7 +56,7 @@ const transporter = nodemailer.createTransport({
 	},
 });
 
-const changelog = await getChangelog();
+const changelog = await getChangeLogFromNote() ?? await getChangelog();
 const mailOptions = {
 	from: config.from,
 	to: config.to,
@@ -43,7 +66,7 @@ const mailOptions = {
 
 transporter.sendMail(mailOptions, (error, info) => {
 	if (error) {
-		throw error;
+		throw new Error(error);
 	}
 
 	console.log('Email sent', info.messageId, info.response, info.envelope);
