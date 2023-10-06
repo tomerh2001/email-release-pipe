79221acc1c2cd3b0652563dea2b5b6093fa8345a
Tomer Horowitz <tomerh@realcommerce.co.il>
2023-10-06 19:35:21 +0300

    feat: added commander

--- index.ts
+++ index.ts
@@ -1,76 +1,76 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable import/no-extraneous-dependencies */
 
-import process from 'node:process';
-import Bun from 'bun';
+import {join} from 'node:path';
+import {Command} from 'commander';
+import nodemailer from 'nodemailer';
 import {marked} from 'marked';
 import {emojify} from 'node-emoji';
-import nodemailer from 'nodemailer';
-import git from 'simple-git';
-import {name, version} from './package.json';
+import Bun from 'bun';
+
+const program = new Command();
+program.requiredOption('-u, --username <type>', 'Username for authentication with the email server')
+	.requiredOption('-p, --password <type>', 'Password for authentication with the email server')
+	.requiredOption('-f, --from <type>', 'Sender\'s email address')
+	.requiredOption('-t, --to <type>', 'Recipient\'s email address')
+	.requiredOption('--base-path <type>', 'Base path for relative paths')
+	.option('-n, --package-name <type>', 'The name of the package being released')
+	.option('-v, --pkg-version <type>', 'The version of the package being released')
+	.option('--ssl-verify <boolean>', 'Whether or not to verify the SSL certificate of the email server', false)
+	.option('--changelog-file <type>', 'The path of the CHANGELOG.md file', 'CHANGELOG.md')
+	.option('-s, --subject <type>', 'The subject line to use for the release email')
+	.parse(Bun.argv);
+
+const options = program.opts();
 
 /**
- * Returns an object containing configuration options for the email release pipe.
- * @returns An object containing the following properties:
- * - `packageName`: The name of the package being released.
- * - `version`: The version of the package being released.
- * - `username`: The username to use for authentication with the email server.
- * - `password`: The password to use for authentication with the email server.
- * - `from`: The email address to use as the sender.
- * - `to`: The email address to use as the recipient.
- * - `sslVerify`: Whether or not to verify the SSL certificate of the email server.
- * - `subject`: The subject line to use for the release email.
+ * Retrieves the changelog from a file and returns it as a Markdown string with emojis.
+ * @param basePath - The base path of the file.
+ * @param filePath - The path of the file relative to the base path.
+ * @returns The changelog as a Markdown string with emojis.
  */
-export function getConfig() {
-	return {
-		packageName: process.env.PACKAGE_NAME ?? name,
-		version: process.env.VERSION ?? version,
-		username: process.env.USERNAME,
-		password: process.env.PASSWORD,
-		from: process.env.FROM,
-		to: process.env.TO,
-		sslVerify: process.env.SSL_VERIFY ?? false,
-		changelogFile: process.env.CHANGELOG_FILE ?? 'CHANGELOG.md',
-		subject: process.env?.SUBJECT ?? `Release v${process.env.VERSION ?? version} for ${process.env.PACKAGE_NAME ?? name}`,
-	};
-}
-
-/**
- * Retrieves the contents of the CHANGELOG.md file, converts it to HTML using the
- * `marked` library, and applies emoji replacements using the `emojify` library.
- * @returns A Promise that resolves to the HTML content of the CHANGELOG.md file with
- * emoji replacements applied.
- */
-export async function getChangelog() {
-	const changelog = await Bun.file(config.changelogFile).text();
+async function getChangelog(basePath: string, filePath: string) {
+	const changelog = await Bun.file(join(basePath, filePath)).text();
 	return marked(emojify(changelog));
 }
 
-const config = getConfig();
-console.debug('Configuration:', config);
+/**
+ * Sends an email with the changelog for a package release.
+ * @returns A Promise that resolves when the email has been sent.
+ */
+async function sendEmail() {
+	const packageJson = await Bun.file(join(options.basePath, 'package.json')).json();
+	const {name, version} = packageJson;
 
-const simpleGit = git({config: [`http.sslVerify=${config.sslVerify}`]});
-const transporter = nodemailer.createTransport({
-	service: 'gmail',
-	auth: {
-		user: config.username,
-		pass: config.password,
-	},
-});
+	const subject = options.subject ?? `Release v${options.pkgVersion || version} for ${options.packageName || name}`;
+	const transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: options.username,
+			pass: options.password,
+		},
+		tls: {
+			rejectUnauthorized: options.sslVerify,
+		},
+	});
 
-const changelog = await getChangelog();
-const mailOptions = {
-	from: config.from,
-	to: config.to,
-	subject: config.subject,
-	html: changelog,
-};
+	const changelog = await getChangelog(options.basePath, options.changelogFile);
+	const mailOptions = {
+		from: options.from,
+		to: options.to,
+		subject,
+		html: changelog,
+	};
+	transporter.sendMail(mailOptions, (error, info) => {
+		if (error) {
+			console.error(error);
+			return;
+		}
 
-transporter.sendMail(mailOptions, (error, info) => {
-	if (error) {
-		throw new Error(error);
-	}
+		console.log('Email sent', info.messageId, info.response, info.envelope);
+	});
+}
 
-	console.log('Email sent', info.messageId, info.response, info.envelope);
-});
+await sendEmail();
